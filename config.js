c4e98cc85cae7c62a5bf4fddc586eb8f06bee24d
Brian Vanko <brianvanko@gmail.com>
2015-02-03 16:39:52 -0800

    added mongolab uri

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 module.exports = {
 	'port': process.env.PORT || 3000,
-	'database': 'mongodb://localhost/api_app',
+	'database': 'mongodb://admin:password@ds041561.mongolab.com:41561/buynowclub',
 	'secret': 'supersecrettoken'
 };
+
+//'database': 'mongodb://localhost/api_app',
