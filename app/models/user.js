6dba87ce381f16cd450cd50e16f1e17c31ef5873
Brian Vanko <brianvanko@gmail.com>
2015-02-04 00:13:47 -0800

    fixed typo.

--- app/models/user.js
+++ app/models/user.js
@@ -5,14 +5,14 @@ var bcrypt 		 = require('bcrypt-nodejs');
 var UserSchema   = new Schema({
 	name: String,
 	username: { type: String, required: true, index: { unique: true }},
-	password: { type: String, required: true, select: false }
-	favorites: { type: Array, index: { unique: true}}
+	password: { type: String, required: true, select: false },
+	favorites: { type: Array }
 });
 
 UserSchema.pre('save', function(next) {
 	var user = this;
 
-	if (!user.isModified('password')) return next();
+	if (!user.isModified('password')) return next ();
 
 	bcrypt.hash(user.password, null, null, function(err, hash) {
 		if (err) return next(err);
